953875308a605930fa3079aa454a2dbadd3830d2
Steeve Morin <steeve.morin@gmail.com>
2014-11-20 15:23:01 +0100

    Refactor some endpoints
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from pulsar.main import main
+from pulsar import service
 
 
-main()
+service.run()
